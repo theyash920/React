9ca9d1216a0818be13b97efc577013e05db99b36
Yash Chopra <theyash920@gmail.com>
2025-07-13 23:37:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
-  const [value,setValue] = useState(0)
+  const [value,setValue] = useState(7)
 
   return(
     <div className = "App">
       <Navbar/>
       <div className = 'value'>{value}</div>
-      <button onClick={()=>{setValue(value + 1)}}>Click me</button>
+      <button onClick={()=>{setValue(value + 1)}}>Increment Counter</button>
       <Footer/>
     </div>
   );
